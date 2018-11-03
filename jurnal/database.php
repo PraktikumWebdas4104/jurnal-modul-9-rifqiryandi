7fca23ed35c45d3826edb58efa203487d0a6b435
Rifqi ryandi <42662867+rifqiryandi@users.noreply.github.com>
2018-11-03 11:34:22 +0700

    Add files via upload

--- jurnal/database.php
+++ jurnal/database.php
@@ -5,16 +5,16 @@ class database{
 	var $host = "localhost";
 	var $uname = "root";
 	var $pass = "";
-	var $db = ""; //isi sesuai nama database anda
+	var $db = "jurnal9"; //isi sesuai nama database anda
 
 	function __construct(){
-		$this->conn = //buatlah koneksi secara OOP
+		$this->conn = mysqli_connect($this->host,$this->uname,$this->pass,$this->db);//buatlah koneksi secara OOP
 		
 	}
 
 	function tampil_data(){
 		//lengkapilah method tampil data
-		//query select user
+		$data = mysqli_query($this->conn,"SELECT * FROM data");//query select user
 
 		while($d = mysqli_fetch_array($data)){
 			$hasil[] = $d;
@@ -24,18 +24,23 @@ class database{
 	}
 
 	function input($nama,$alamat,$usia){
-		//buatlah method input
-		//query inset into user
+		$que = mysqli_query($this->conn,"INSERT INTO data(nama, alamat, usia, id) VALUES ('$nama','$alamat','$usia','')");//buatlah method input
+		if ($$que) {
+			echo "Berhasil";
+		}//query inset into user
 	}
 
 	function hapus($id){
-		//buatlah method hapus
+		$que = mysqli_query($this->conn,"DELETE FROM data WHERE id='$id'");//buatlah method input
+		if ($$que) {
+			echo "Berhasil";
+		}//buatlah method hapus
 		//query delete from id where id ='$id'
 	}
 
 	function edit($id){
 		//lengkapilah method edit
-		//query select from user where id ='$id'
+		$data = mysqli_query($this->conn,"SELECT * FROM data WHERE id='$id'");//query select from user where id ='$id'
 		while($d = mysqli_fetch_array($data)){
 			$hasil[] = $d;
 		}
@@ -43,8 +48,10 @@ class database{
 	}
 
 	function update($id,$nama,$alamat,$usia){
-		//buatlah method update
-		//query update user set blablabla where id='$id'
+		$que = mysqli_query($this->conn,"UPDATE data SET nama='$nama',alamat='$alamat',usia='$usia' WHERE id='$id'");//buatlah method update
+		if ($que) {
+			echo "berhasil";# code...
+		}//query update user set blablabla where id='$id'
 	}
 
 } 
